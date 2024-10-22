2deafe479fd1139583afc55b2900a4873b61e054
skullonrisk <nazmullabib012@gmail.com>
2024-10-22 16:44:16 +0600

    Formatted

--- Day 01/Q_Work_Smarter_Not_Harder.cpp
+++ Day 01/Q_Work_Smarter_Not_Harder.cpp
@@ -1,36 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void test(){
-    float l,v1,v2;
-    cin>>l>>v1>>v2;
-    float T=l/v1;
-    float H=l/v2;
-    int t=T;
-    int h=H;
-    if(T-(int)T>0){
-        t=t+1;
+void test()
+{
+    float l, v1, v2;
+    cin >> l >> v1 >> v2;
+    float T = l / v1;
+    float H = l / v2;
+    int t = T;
+    int h = H;
+    if (T - (int)T > 0)
+    {
+        t = t + 1;
     }
-    if(H-(int)H>0){
-        h=h+1;
+    if (H - (int)H > 0)
+    {
+        h = h + 1;
     }
-    if(t>h){
-        if(t-h>1){
-            cout<<t-h-1<<"\n";
+    if (t > h)
+    {
+        if (t - h > 1)
+        {
+            cout << t - h - 1 << "\n";
         }
         else
-        cout<<0<<"\n";
+            cout << 0 << "\n";
     }
     else
-    cout<<-1<<"\n";
-  
-
-
-
+        cout << -1 << "\n";
 }
-int main(){
+int main()
+{
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         test();
     }
 }
