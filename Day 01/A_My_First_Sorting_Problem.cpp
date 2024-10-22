90b038f9cd9cb21e5b54fe0cd1a3dac4dfd84781
skullonrisk <nazmullabib012@gmail.com>
2024-10-22 16:50:08 +0600

    second problem

---
+++ Day 01/A_My_First_Sorting_Problem.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void test(){
+    int arr[2];
+    for(int i=0;i<2;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+2);
+    for(int i=0;i<2;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\n";
+
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        test();
+    }
+}
